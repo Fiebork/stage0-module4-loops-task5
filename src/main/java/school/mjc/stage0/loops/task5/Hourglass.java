87553f270893b73881129801d5413e6aeb8634b0
Bilbo3340 <vladik.elce1@gmail.com>
2022-09-01 13:43:13 +0300

    upt

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,56 @@
 package school.mjc.stage0.loops.task5;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int n = height / 2;
+        boolean isDone = false;
+        if (height % 2 == 0) {
+            for (int i = 1; i <= height; i++) {
+                StringBuilder s = new StringBuilder();
+                for (int j = 0; j < height / 2; j++) {
+                    if (j < n) {
+                        s.append("8");
+                    } else {
+                        s.append(" ");
+                    }
+                }
+                if (n == 1 && !isDone) {
+                    isDone = true;
+                    n--;
+                }
+                if (!isDone) {
+                    n--;
+                } else {
+                    n++;
+                }
 
+                s.reverse();
+                System.out.println(s.toString() + s.reverse().toString());
+            }
+        } else {
+            for (int i = 1; i <= height; i++) {
+                StringBuilder s = new StringBuilder();
+                for (int j = 0; j < height / 2; j++) {
+                    if (j < n) {
+                        s.append("8");
+                    } else {
+                        s.append(" ");
+                    }
+                }
+                if (n == 0 && !isDone) {
+                    isDone = true;
+                }
+                if (!isDone) {
+                    n--;
+                } else {
+                    n++;
+                }
+                s.reverse();
+                System.out.println(s.toString() + "8" + s.reverse().toString());
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.loops.task5;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        String s = "8";
+        for (int i = 1; i <= cathetusLength; i++) {
+            String sRepeated = IntStream.range(0, i).mapToObj(a -> s).collect(Collectors.joining(""));
+            System.out.println(sRepeated);
+        }
     }
 }
